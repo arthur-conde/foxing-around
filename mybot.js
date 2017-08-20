e39d9df150c708bed189cda217ea684a2d418990
Timo <timoqueezle@gmail.com>
2017-08-20 14:32:47 +0200

    add !prefix
    change prefix via command by extracting the new prefix form a given userinput and storing it in the config.json via native fs

--- mybot.js
+++ mybot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const fs = require("fs");
 
 client.login("config.token");
 
@@ -18,5 +19,13 @@ client.on("message", (message) => {
 
   if (message.content.startsWith(config.prefix + "foo")) {
     message.channel.send("bar!");
+  } else {
+  if (message.content.startsWith(config.prefix + "prefix")) {
+      // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
+      let newPrefix = message.content.split(" ").slice(1, 2)[0];
+      //change the configuration in memory
+      config.prefix = newPrefix
+      //saving the file
+      fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
   }
 });
