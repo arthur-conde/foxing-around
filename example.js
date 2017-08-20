423773c4fee87f551b9d99837f07061c902ec96b
Timo <timoqueezle@gmail.com>
2017-08-20 02:46:22 +0200

    grabbed a package.json.. somewhere
    need to invesiagate what this package,json does at some point.

--- example.js
+++ example.js
@@ -1,10 +1,20 @@
-var bot = new Eris("MzQ4NjA3Nzk2MzM1NjA3ODE3.DHpbAg.YG-n5gjkU4mxUrAKlvm85tROGRI");
-bot.on("ready", () => {
-    console.log("Ready!");
+const Eris = require("eris");
+
+var bot = new Eris("MzQ4NjA3Nzk2MzM1NjA3ODE3.DHproQ.GDTQYjofrJ32q77XHMYnXBEd2_c");
+// Replace BOT_TOKEN with your bot account's token
+
+bot.on("ready", () => { // When the bot is ready
+    console.log("Ready!"); // Log "Ready!"
 });
-bot.on("messageCreate", (msg) => {
-    if(msg.content === "!ping") {
+
+bot.on("messageCreate", (msg) => { // When a message is created
+    if(msg.content === "!ping") { // If the message content is "!ping"
         bot.createMessage(msg.channel.id, "Pong!");
+        // Send a message in the same channel with "Pong!"
+    } else if(msg.content === "!pong") { // Otherwise, if the message is "!pong"
+        bot.createMessage(msg.channel.id, "Ping!");
+        // Respond with "Ping!"
     }
 });
-bot.connect();
+
+bot.connect(); // Get the bot to connect to Discord
