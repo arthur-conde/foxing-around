65b6f1e60d74814fe7ac547795dc7a0f78eb7b7e
Timo <timoqueezle@gmail.com>
2017-08-20 23:59:23 +0200

    now with less stupidity
    cleaned things up i guess

--- mybot.js
+++ mybot.js
@@ -1,16 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
-const fs = require("fs");
-
-client.login(config.token);
-
-client.on("ready", () => {
-  console.log("I am ready!");
-});
-
-const Discord = require("discord.js");
-const client = new Discord.Client();
 const fs = require("fs");
 
 const config = require("./config.json");
