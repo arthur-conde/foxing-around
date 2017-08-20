90219208e4acbfb296f2e452c86f29f8cc803598
Timo <timoqueezle@gmail.com>
2017-08-20 15:02:51 +0200

    fix syntax error
    fix syntax error, bc Souji is a dumb

--- mybot.js
+++ mybot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const config = require("./config.json");
 const fs = require("fs");
 
-client.login("config.token");
+client.login(config.token);
 
 client.on("ready", () => {
   console.log("I am ready!");
@@ -20,13 +20,15 @@ client.on("message", (message) => {
   if (message.content.startsWith(config.prefix + "foo")) {
     message.channel.send("bar!");
   } else {
-  if (message.content.startsWith(config.prefix + "prefix")) {
-      if(message.author.id !== config.ownerID) return;
-      // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
-      let newPrefix = message.content.split(" ").slice(1, 2)[0];
-      //change the configuration in memory
-      config.prefix = newPrefix
-      //saving the file
-      fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
+    if(message.content.startsWith(config.prefix + "prefix")) {
+    if(message.author.id !== config.ownerID) return;
+    // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
+    let newPrefix = message.content.split(" ").slice(1, 2)[0];
+    // change the configuration in memory
+    config.prefix = newPrefix;
+    message.channel.send("Okay " + message.author.username + ", prefix changed to " + "`" + newPrefix + "`");
+
+    // Now we have to save the file.
+    fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
   }
-});
+}})
