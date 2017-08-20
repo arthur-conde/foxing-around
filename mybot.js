76dca643873cd158ada902a89e139e7f1412ba6b
Timo <timoqueezle@gmail.com>
2017-08-20 23:55:38 +0200

    testing command handler
    that thing looks awesome.. also fricking intimidating...

--- mybot.js
+++ mybot.js
@@ -9,6 +9,43 @@ client.on("ready", () => {
   console.log("I am ready!");
 });
 
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+
+const config = require("./config.json");
+
+// This loop reads the /events/ folder and attaches each event file to the appropriate event.
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    let eventFunction = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    // super-secret recipe to call events with all their proper arguments *after* the `client` var.
+    client.on(eventName, (...args) => eventFunction.run(client, ...args));
+  });
+});
+
+client.on("message", message => {
+  if (message.author.bot) return;
+  if(message.content.indexOf(config.prefix) !== 0) return;
+
+  // This is the best way to define args. Trust me.
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  // The list of if/else is replaced with those simple 2 lines:
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+    commandFile.run(client, message, args);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+client.login(config.token);
+
+/*
 client.on("message", (message) => {
   //Exit and stop if it's not there
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
@@ -32,7 +69,7 @@ client.on("message", (message) => {
     fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
   }
 }})
-
+*/
 client.on("error", (e) => console.error(e));
 client.on("warn", (e) => console.warn(e));
 client.on("debug", (e) => console.info(e));
