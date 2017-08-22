02667a7e7af4df5697839d3e4a6f1da905f2b2bf
Timo <timoqueezle@gmail.com>
2017-08-23 00:02:56 +0200

    refreshed with guide code
    let's try that again i guess
    You are filled with DETERMINATION

--- mybot.js
+++ mybot.js
@@ -33,32 +33,3 @@ client.on("message", message => {
 });
 
 client.login(config.token);
-
-/*
-client.on("message", (message) => {
-  //Exit and stop if it's not there
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-
-  if (message.content.startsWith(config.prefix + "ping")) {
-    message.channel.send("pong!");
-  }  else
-
-  if (message.content.startsWith(config.prefix + "foo")) {
-    message.channel.send("bar!");
-  } else {
-    if(message.content.startsWith(config.prefix + "prefix")) {
-    if(message.author.id !== config.ownerID) return;
-    // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
-    let newPrefix = message.content.split(" ").slice(1, 2)[0];
-    // change the configuration in memory
-    config.prefix = newPrefix;
-    message.channel.send("Okay " + message.author.username + ", prefix changed to " + "`" + newPrefix + "`");
-
-    // Now we have to save the file.
-    fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
-  }
-}})
-*/
-client.on("error", (e) => console.error(e));
-client.on("warn", (e) => console.warn(e));
-client.on("debug", (e) => console.info(e));
