a7f40e04004f0e57c0eda463bda48812553a92e0
Timo <timoqueezle@gmail.com>
2017-08-23 00:18:14 +0200

    remove return from logging
    duh.

--- commands/kick.js
+++ commands/kick.js
@@ -1,19 +1,19 @@
 exports.run = (client, message, args) => {
   return message.reply("command started")
-  const modRole = message.guild.roles.find("name", "Mods");
-  return message.reply("Searching for Modrole...")
+  // const modRole = message.guild.roles.find("name", "Mods");
+  message.reply("Searching for Modrole...")
   if (!modRole)
     return console.log("The Mods role does not exist");
 
   if (!message.member.roles.has(modRole.id))
-    return message.reply("You can't use this command.");
+    message.reply("You can't use this command.");
 
   if (message.mentions.users.size === 0) {
-    return message.reply("Please mention a user to kick");
+    message.reply("Please mention a user to kick");
 
     if (!message.guild.me.hasPermission("KICK_MEMBERS"))
       return message.reply("");
-      return message.reply("you lack kicking perms")
+      message.reply("you lack kicking perms")
 
     const kickMember = message.mentions.members.first();
 
