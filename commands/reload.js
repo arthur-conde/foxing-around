910801d4aee0c57e2adb878bc0e1b9979ae59e91
Timo <timoqueezle@gmail.com>
2017-08-24 11:50:49 +0200

    embedify reload
    embedify reload

--- commands/reload.js
+++ commands/reload.js
@@ -2,5 +2,7 @@ exports.run = (client, message, args) => {
   if(!args || args.size < 1) return message.reply("Must provide a command name to reload.");
   // the path is relative to the *current folder*, so just ./filename.js
   delete require.cache[require.resolve(`./${args[0]}.js`)];
-  message.reply(`The command ${args[0]} has been reloaded`);
+  message.channel.send(
+    {embed:{color: message.guild.me.displayColor, description: `:white_check_mark: The command ${args[0]} has been reloaded`}}
+  );
 };
