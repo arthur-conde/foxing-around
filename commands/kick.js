c0c3ae2a722598295890872ae2e95ff88898315c
Timo <timoqueezle@gmail.com>
2017-08-24 12:23:48 +0200

    embedify kick
    embedify kick, add name + id to success mesage

--- commands/kick.js
+++ commands/kick.js
@@ -1,21 +1,20 @@
 exports.run = (client, message, [mention, ...reason]) => {
   const config = require("../config.json");
   if (!message.guild.me.hasPermission("KICK_MEMBERS")) {
-    message.reply("you lack kicking perms")
-    return message.reply("");
+    return message.channel.send({embed:{color: message.guild.me.displayColor, description: ":x: I don't have the permissions to do that"}});
   }
   if (message.mentions.users.size === 0) {
-    return message.reply("Please mention a user to kick");
+    return message.channel.send({embed:{color: message.guild.me.displayColor, description: ":x: Please mention a user to kick"}});
   } else {
   const kickMember = message.mentions.members.first();
     if (message.author.id === kickMember.id) {
-      return message.reply(`You can't kick yourself.`);
+      return message.channel.send({embed:{color: message.guild.me.displayColor, description: ":x: You can't kick yourself"}});
     }
     if (config.ownerID === kickMember.id) {
-      return message.reply(`You can't kick ` + message.guild.members.get("83886770768314368").displayName + ` ! -_-"`);
+      return message.channel.send({embed:{color: message.guild.me.displayColor, description: `:x: You can't kick ${message.guild.members.get("83886770768314368").displayName}`}});
     }
     kickMember.kick(reason.join(" ")).then(member => {
-      message.reply(`${member.user.username} was succesfully kicked.`);
+      message.channel.send({embed:{color: message.guild.me.displayColor, description: `:white_check_mark: ${kickMember.displayName} (ID: ${kickMember.id}) was succesfully kicked.`}});
     });
   }
 };
