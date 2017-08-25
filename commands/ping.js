cf011f974da758227c27c9ee80c2577a6e8e5d39
Timo <timoqueezle@gmail.com>
2017-08-25 23:20:22 +0200

    boldify pong for more boldness!

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
   message.channel.send(
-    {embed:{color: message.guild.me.displayColor, description: `:ping_pong: pong! (${client.ping.toFixed(0)} ms)`}}
+    {embed:{color: message.guild.me.displayColor, description: `:ping_pong: pong! (**${client.ping.toFixed(0)} ms)**`}}
   );
 }
