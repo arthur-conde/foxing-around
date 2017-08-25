4a51025ce1d5b938d496abe6f625845fab086503
Timo <timoqueezle@gmail.com>
2017-08-25 12:17:24 +0200

    make keck work
    second option for find() to check for nick or username and loop through guild.members collection until things are found... or not
    Keck now works with
    Mention, ID, Nickname, Username (the two latter only without space inbetween)

--- commands/keck.js
+++ commands/keck.js
@@ -1,9 +1,5 @@
 exports.run = (client, message, [mention, ...reason]) => {
   const config = require("../config.json");
-  //message.channel.send(`${message}`);
-  //message.channel.send(`${mention}`);
-  //message.channel.send(`${reason}`);
-
 
   if (!message.guild.me.hasPermission("KICK_MEMBERS")) {
     return message.channel.send({embed:{color: message.guild.me.displayColor, description: ":x: I don't have the permissions to do that"}});
@@ -16,19 +12,11 @@ exports.run = (client, message, [mention, ...reason]) => {
       const kickMember = message.guild.members.get(`${mention}`);
       return message.channel.send({embed:{color: message.guild.me.displayColor, description: `:white_check_mark: ${kickMember.displayName} | <@${kickMember.id}> | (ID: ${kickMember.id}) was succesfully kicked.`}});
    }
-   var nickUserName = message.guild.members.find(member => (member.nickname == null ? member.user.username : member.nickname) == `${mention}`);
+   var nickUserName = message.guild.members.find(member => (member.nickname != `${mention}` ? member.user.username : member.nickname) == `${mention}`);
    if (nickUserName != null) {
       const kickMember = nickUserName;
-      return message.channel.send({embed:{color: message.guild.me.displayColor, description: `:white_check_mark: ${kickMember.displayName} | <@${kickMember.id}> | (ID: ${kickMember.id}) was succesfully kicked.`}});
-    }
-  /*  if (message.guild.members.find("nickname",`${mention}`) !== null) {
-       const kickMember = message.guild.members.find("nickname",`${mention}`);
-       return message.channel.send({embed:{color: message.guild.me.displayColor, description: `:white_check_mark: ${kickMember.displayName} | <@${kickMember.id}> | (ID: ${kickMember.id}) was succesfully kicked.`}});
-   }*/ else {
-    message.channel.send({embed:{color: message.guild.me.displayColor, description: `:x: Please specify a guild member`}})
+      return message.channel.send({embed:{color: message.guild.me.displayColor, description: `:white_check_mark: ${nickUserName.displayName} | <@${nickUserName.id}> | (ID: ${nickUserName.id}) was succesfully kicked.`}});
+    } else {
+    message.channel.send({embed:{color: message.guild.me.displayColor, description: `:x: Invalid guild member`}})
    }
 };
-
-// console.log(message.guild.members.find(member => (member.nickname == null ? member.user.username : member.nickname) == `${mention}`))
-
-// console.log(message.guild.members.find(member => (member.user.username) == "Souji"))
--- commands/kick.js
+++ commands/kick.js
@@ -6,15 +6,15 @@ exports.run = (client, message, [mention, ...reason]) => {
   if (message.mentions.users.size === 0) {
     return message.channel.send({embed:{color: message.guild.me.displayColor, description: ":x: Please mention a user to kick"}});
   } else {
-  const kickMember = message.mentions.members.first();
-    if (message.author.id === kickMember.id) {
-      return message.channel.send({embed:{color: message.guild.me.displayColor, description: ":x: You can't kick yourself"}});
-    }
-    if (config.ownerID === kickMember.id) {
-      return message.channel.send({embed:{color: message.guild.me.displayColor, description: `:x: You can't kick ${message.guild.members.get("83886770768314368").displayName}`}});
-    }
-    kickMember.kick(reason.join(" ")).then(member => {
-      message.channel.send({embed:{color: message.guild.me.displayColor, description: `:white_check_mark: ${kickMember.displayName} | <@${kickMember.id}> | (ID: ${kickMember.id}) was succesfully kicked.`}});
-    });
+      const kickMember = message.mentions.members.first();
+      if (message.author.id === kickMember.id) {
+        return message.channel.send({embed:{color: message.guild.me.displayColor, description: ":x: You can't kick yourself"}});
+      }
+      if (config.ownerID === kickMember.id) {
+        return message.channel.send({embed:{color: message.guild.me.displayColor, description: `:x: You can't kick ${message.guild.members.get("83886770768314368").displayName}`}});
+      }
+      kickMember.kick(reason.join(" ")).then(member => {
+        message.channel.send({embed:{color: message.guild.me.displayColor, description: `:white_check_mark: ${kickMember.displayName} | <@${kickMember.id}> | (ID: ${kickMember.id}) was succesfully kicked.`}});
+      });
   }
 };
