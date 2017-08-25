52478ef5451cb0c84c5bca92603c089623e3135b
Timo <timoqueezle@gmail.com>
2017-08-25 23:04:47 +0200

    caseinsensitivity + nick/username for !kick

--- commands/kick.js
+++ commands/kick.js
@@ -1,20 +1,35 @@
 exports.run = (client, message, [mention, ...reason]) => {
   const config = require("../config.json");
+
   if (!message.guild.me.hasPermission("KICK_MEMBERS")) {
     return message.channel.send({embed:{color: message.guild.me.displayColor, description: ":x: I don't have the permissions to do that"}});
   }
-  if (message.mentions.users.size === 0) {
-    return message.channel.send({embed:{color: message.guild.me.displayColor, description: ":x: Please mention a user to kick"}});
-  } else {
+   if (message.mentions.users.size === 1) {
       const kickMember = message.mentions.members.first();
-      if (message.author.id === kickMember.id) {
-        return message.channel.send({embed:{color: message.guild.me.displayColor, description: ":x: You can't kick yourself"}});
-      }
-      if (config.ownerID === kickMember.id) {
-        return message.channel.send({embed:{color: message.guild.me.displayColor, description: `:x: You can't kick ${message.guild.members.get("83886770768314368").displayName}`}});
-      }
       kickMember.kick(reason.join(" ")).then(member => {
-        message.channel.send({embed:{color: message.guild.me.displayColor, description: `:white_check_mark: ${kickMember.displayName} | <@${kickMember.id}> | (ID: ${kickMember.id}) was succesfully kicked.`}});
-      });
-  }
+        return message.channel.send({embed:{color: message.guild.me.displayColor, description: `:white_check_mark: ${kickMember.displayName} | <@${kickMember.id}> | (ID: ${kickMember.id}) was succesfully kicked.`
+      }})});
+   }
+   if (message.guild.members.get(`${mention}`) !== undefined) {
+      const kickMember = message.guild.members.get(`${mention}`);
+      kickMember.kick(reason.join(" ")).then(member => {
+        return message.channel.send({embed:{color: message.guild.me.displayColor, description: `:white_check_mark: ${kickMember.displayName} | <@${kickMember.id}> | (ID: ${kickMember.id}) was succesfully kicked.`
+      }})});
+   }
+   var nickUserName = message.guild.members.find(function (member) {
+    if (member.nickname != null)
+    {
+        if (member.nickname.toUpperCase() == mention.toUpperCase())
+            return true;
+    }
+    if (member.user.username.toUpperCase() == mention.toUpperCase())
+        return true;
+})
+   if (nickUserName != null) {
+     nickUserName.kick(reason.join(" ")).then(member => {
+       return message.channel.send({embed:{color: message.guild.me.displayColor, description: `:white_check_mark: ${nickUserName.displayName} | <@${nickUserName.id}> | (ID: ${nickUserName.id}) was succesfully kicked.`
+     }})});
+    } else {
+    message.channel.send({embed:{color: message.guild.me.displayColor, description: `:x: Invalid guild member`}})
+   }
 };
