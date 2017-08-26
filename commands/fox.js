382bd4925e3edf6c5a32e062467cc685ac695143
Timo <timoqueezle@gmail.com>
2017-08-26 14:24:38 +0200

    add delete for invoke of !fox

--- commands/fox.js
+++ commands/fox.js
@@ -1,5 +1,6 @@
 exports.run = (client, message) => {
   const ranfoxconfig = require("../ranfoxconfig.json");
   var foximg = ranfoxconfig.foxes[Math.floor(Math.random() * ranfoxconfig.foxes.length)];
-  message.channel.send({embed:{color: message.guild.me.displayColor, image: {url: foximg} }});
+  message.channel.send({embed:{color: message.guild.me.displayColor,description:` Here is the fox <@${message.member.id}> asked for:`, image: {url: foximg} }});
+  message.delete(1000);
 }
