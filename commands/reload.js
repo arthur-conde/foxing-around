e9693d5db03e41348ee94ccd8ad5fedb2396c7fa
Timo <timoqueezle@gmail.com>
2017-08-26 14:52:53 +0200

    messagedelete for !reload

--- commands/reload.js
+++ commands/reload.js
@@ -1,15 +1,23 @@
 exports.run = (client, message, args) => {
-  if(!args || args.size < 1) return message.reply({embed:{color: message.guild.me.displayColor, description: `:x: Provide a command to reload`}});
+  if(!args || args.length < 1) {
+    message.channel.send({embed:{color: message.guild.me.displayColor, description: `:x: <@${message.member.id}>, please provide a command to reload`}}).then( message => {
+      message.guild.me.lastMessage.delete(4000);
+    });
+    message.delete(1000);
+    return;
+  }
   if (args == "foxes") {
     delete require.cache[require.resolve(`../ranfoxconfig.json`)];
-    message.channel.send(
-      {embed:{color: message.guild.me.displayColor, description: `:fox: Foxes have been reloaded`}}
-    );
+    message.channel.send({embed:{color: message.guild.me.displayColor, description: `:fox: Foxes have been reloaded`}}).then( message => {
+        message.guild.me.lastMessage.delete(4000);
+    });
+    message.delete(1000);
     return;
   }
   // the path is relative to the *current folder*, so just ./filename.js
   delete require.cache[require.resolve(`./${args[0]}.js`)];
-  message.channel.send(
-    {embed:{color: message.guild.me.displayColor, description: `:white_check_mark: The command ${args[0]} has been reloaded`}}
-  );
+  message.channel.send({embed:{color: message.guild.me.displayColor, description: `:white_check_mark: Okay, <@${message.member.id}> the command **${args[0]}** has been reloaded`}}).then( message => {
+    message.guild.me.lastMessage.delete(4000);
+  });
+  message.delete(1000)
 };
