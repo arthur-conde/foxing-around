09d2663b8c556b8eda03a7c921d6a8b93f0a2325
Timo <timoqueezle@gmail.com>
2017-08-26 12:18:05 +0200

    add ability to reload foxes

--- commands/reload.js
+++ commands/reload.js
@@ -1,5 +1,12 @@
 exports.run = (client, message, args) => {
-  if(!args || args.size < 1) return message.reply("Must provide a command name to reload.");
+  if(!args || args.size < 1) return message.reply({embed:{color: message.guild.me.displayColor, description: `:x: Provide a command to reload`}});
+  if (args == "foxes") {
+    delete require.cache[require.resolve(`../ranfoxconfig.json`)];
+    message.channel.send(
+      {embed:{color: message.guild.me.displayColor, description: `:fox: Foxes have been reloaded`}}
+    );
+    return;
+  }
   // the path is relative to the *current folder*, so just ./filename.js
   delete require.cache[require.resolve(`./${args[0]}.js`)];
   message.channel.send(
