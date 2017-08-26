876a46fa4af285f2eabbeddf3ca596d8dec4c7b0
Timo <timoqueezle@gmail.com>
2017-08-27 00:31:15 +0200

    info with lots of things, posts info.

--- commands/info.js
+++ commands/info.js
@@ -1,7 +1,88 @@
-exports.run = (client, message, args) => {
+exports.run = (client, message, [mention, ...superfluous]) => {
   const config = require("../config.json");
+  /*
+  console.log(mention);
+  if (mention == message.guild.id || "guild" || "server") {
+    console.log("u ask for server");
+  }*/
 
-   if (message.mentions.users.size === 1) {
+  //-------------------------------
+/*  function postInfo (infoSubject) {
+    const isBot = infoSubject.user.bot == true ? "[BOT]" : "";
+    const infoSubjectNickname = infoSubject.nickname != null ? `${infoSubject.nickname}` : "None"
+    const infoSubjectStatus = infoSubject.user.presence.status.charAt(0).toUpperCase() + infoSubject.user.presence.status.slice(1);      var infoSubjectAccountAge = Math.floor((Date.now() - infoSubject.user.createdAt) / 1000 / 60 / 60 / 24);
+    var infoSubjectMemberAge = Math.floor((Date.now() - infoSubject.joinedAt) / 1000 / 60 / 60 / 24);
+    const infoSubjectPlaying = infoSubject.user.presence.game != null ? `${infoSubject.user.presence.game.name}` : "None"
+    const infoSubjectCreated = new Date(Date.UTC(infoSubject.user.createdAt));
+    var roleList = [];
+    infoSubject.roles.forEach(role => {if (message.guild.id == role.id) {return;} else {roleList.push(role.name)}});
+    message.channel.send({embed:{
+      color: infoSubject.displayColor,
+      author: {
+        name: `${isBot} ${infoSubject.user.username}#${infoSubject.user.discriminator}`,
+        icon_url: infoSubject.user.avatarURL
+      },
+      title: "ID",
+      description: `${infoSubject.user.id}`,
+      thumbnail: {
+        url: infoSubject.user.avatarURL
+      },
+      fields: [
+        { name: "Mention",
+          value: `<@${infoSubject.user.id}>`,
+          inline: true
+        },
+        { name: "Nickname",
+          value: `${infoSubjectNickname}`,
+          inline: true
+        },
+        { name: "Status",
+          value: `${infoSubjectStatus}`,
+          inline: true
+        },
+        { name: "Playing",
+          value: `${infoSubjectPlaying}`,
+          inline: true
+        },
+        { name: "Accountage",
+          value: `${infoSubjectAccountAge} day(s)`,
+          inline: true
+        },
+        { name: "Memberage",
+          value: `${infoSubjectMemberAge} day(s)`,
+          inline: true
+        },
+        { name: `Server Deafened`,
+          value: `${infoSubject.serverDeaf}`,
+          inline: true
+        },
+        { name: `Server Muted`,
+          value: `${infoSubject.serverMute}`,
+          inline: true
+        },
+        { name: "Created On",
+          value: `${infoSubject.user.createdAt}`,
+          inline: false
+        },
+        { name: "Join Date",
+          value: `${infoSubject.joinedAt}`,
+          inline: false
+        },
+        { name: `Roles [${roleList.length}]`,
+          value: `${roleList.join(", ")}`,
+          inline: false
+        }
+      ]
+    }});
+    message.delete(4000);
+    return;
+    }
+  }
+  //-------------------------------
+  if (message.mentions.users.size === 1) {
+      postInfo(message.mentions.members.first());
+*/
+  if (message.mentions.users.size === 1) {
       const infoMember = message.mentions.members.first();
       const isBot = infoMember.user.bot == true ? "[BOT]" : "";
       const infoMemberNickname = infoMember.nickname != null ? `${infoMember.nickname}` : "None"
@@ -9,16 +90,8 @@ exports.run = (client, message, args) => {
       var infoMemberMemberAge = Math.floor((Date.now() - infoMember.joinedAt) / 1000 / 60 / 60 / 24);
       const infoMemberPlaying = infoMember.user.presence.game != null ? `${infoMember.user.presence.game.name}` : "None"
       const infoMemberCreated = new Date(Date.UTC(infoMember.user.createdAt));
-      // const infoMemberJoined
       var roleList = [];
-      for (i = 0; i < infoMember.roles.array().length; i++) {
-        var rolelist = roleList + roles.Role.name;
-        i =+ 1;
-      }
-      console.log(roleList);
-      console.log(infoMember.user.createdAt);
-      console.log(infoMemberCreated);
-      console.log(infoMemberRoles);
+      infoMember.roles.forEach(role => {if (message.guild.id == role.id) {return;} else {roleList.push(role.name)}});
       message.channel.send({embed:{
         color: infoMember.displayColor,
         author: {
@@ -43,6 +116,10 @@ exports.run = (client, message, args) => {
             value: `${infoMemberStatus}`,
             inline: true
           },
+          { name: "Playing",
+            value: `${infoMemberPlaying}`,
+            inline: true
+          },
           { name: "Accountage",
             value: `${infoMemberAccountAge} day(s)`,
             inline: true
@@ -51,8 +128,12 @@ exports.run = (client, message, args) => {
             value: `${infoMemberMemberAge} day(s)`,
             inline: true
           },
-          { name: "Playing",
-            value: `${infoMemberPlaying}`,
+          { name: `Server Deafened`,
+            value: `${infoMember.serverDeaf}`,
+            inline: true
+          },
+          { name: `Server Muted`,
+            value: `${infoMember.serverMute}`,
             inline: true
           },
           { name: "Created On",
@@ -62,18 +143,85 @@ exports.run = (client, message, args) => {
           { name: "Join Date",
             value: `${infoMember.joinedAt}`,
             inline: false
+          },
+          { name: `Roles [${roleList.length}]`,
+            value: `${roleList.join(", ")}`,
+            inline: false
           }
         ]
       }});
-      }
       message.delete(4000);
       return;
-   /*
+      }
    if (message.guild.members.get(`${mention}`) !== undefined) {
-      const kickMember = message.guild.members.get(`${mention}`);
-      kickMember.kick(reason.join(" ")).then(member => {
-        message.channel.send({embed:{color: message.guild.me.displayColor, description: `:white_check_mark: <@${message.member.id}>, ${kickMember.displayName} | <@${kickMember.id}> | (ID: ${kickMember.id}) was succesfully kicked.`
-      }})});
+      const infoMember = message.guild.members.get(`${mention}`);
+      const isBot = infoMember.user.bot == true ? "[BOT]" : "";
+      const infoMemberNickname = infoMember.nickname != null ? `${infoMember.nickname}` : "None"
+      const infoMemberStatus = infoMember.user.presence.status.charAt(0).toUpperCase() + infoMember.user.presence.status.slice(1);      var infoMemberAccountAge = Math.floor((Date.now() - infoMember.user.createdAt) / 1000 / 60 / 60 / 24);
+      var infoMemberMemberAge = Math.floor((Date.now() - infoMember.joinedAt) / 1000 / 60 / 60 / 24);
+      const infoMemberPlaying = infoMember.user.presence.game != null ? `${infoMember.user.presence.game.name}` : "None"
+      const infoMemberCreated = new Date(Date.UTC(infoMember.user.createdAt));
+      var roleList = [];
+
+      infoMember.roles.forEach(role => {if (message.guild.id == role.id) {return;} else {roleList.push(role.name)}});
+      message.channel.send({embed:{
+        color: infoMember.displayColor,
+        author: {
+          name: `${isBot} ${infoMember.user.username}#${infoMember.user.discriminator}`,
+          icon_url: infoMember.user.avatarURL
+        },
+        title: "ID",
+        description: `${infoMember.user.id}`,
+        thumbnail: {
+          url: infoMember.user.avatarURL
+        },
+        fields: [
+          { name: "Mention",
+            value: `<@${infoMember.user.id}>`,
+            inline: true
+          },
+          { name: "Nickname",
+            value: `${infoMemberNickname}`,
+            inline: true
+          },
+          { name: "Status",
+            value: `${infoMemberStatus}`,
+            inline: true
+          },
+          { name: "Playing",
+            value: `${infoMemberPlaying}`,
+            inline: true
+          },
+          { name: "Accountage",
+            value: `${infoMemberAccountAge} day(s)`,
+            inline: true
+          },
+          { name: "Memberage",
+            value: `${infoMemberMemberAge} day(s)`,
+            inline: true
+          },
+          { name: `Server Deafened`,
+            value: `${infoMember.serverDeaf}`,
+            inline: true
+          },
+          { name: `Server Muted`,
+            value: `${infoMember.serverMute}`,
+            inline: true
+          },
+          { name: "Created On",
+            value: `${infoMember.user.createdAt}`,
+            inline: false
+          },
+          { name: "Join Date",
+            value: `${infoMember.joinedAt}`,
+            inline: false
+          },
+          { name: `Roles [${roleList.length}]`,
+            value: `${roleList.join(", ")}`,
+            inline: false
+          }
+        ]
+      }});
       message.delete(4000);
       return;
    }
@@ -87,9 +235,74 @@ exports.run = (client, message, args) => {
         return true;
 })
    if (nickUserName != null) {
-     nickUserName.kick(reason.join(" ")).then(member => {
-       message.channel.send({embed:{color: message.guild.me.displayColor, description: `:white_check_mark: <@${message.member.id}>, ${nickUserName.displayName} | <@${nickUserName.id}> | (ID: ${nickUserName.id}) was succesfully kicked.`
-     }})});
+     const infoMember = nickUserName;
+     const isBot = infoMember.user.bot == true ? "[BOT]" : "";
+     const infoMemberNickname = infoMember.nickname != null ? `${infoMember.nickname}` : "None"
+     const infoMemberStatus = infoMember.user.presence.status.charAt(0).toUpperCase() + infoMember.user.presence.status.slice(1);      var infoMemberAccountAge = Math.floor((Date.now() - infoMember.user.createdAt) / 1000 / 60 / 60 / 24);
+     var infoMemberMemberAge = Math.floor((Date.now() - infoMember.joinedAt) / 1000 / 60 / 60 / 24);
+     const infoMemberPlaying = infoMember.user.presence.game != null ? `${infoMember.user.presence.game.name}` : "None"
+     const infoMemberCreated = new Date(Date.UTC(infoMember.user.createdAt));
+     var roleList = [];
+
+     infoMember.roles.forEach(role => {if (message.guild.id == role.id) {return;} else {roleList.push(role.name)}});
+     message.channel.send({embed:{
+       color: infoMember.displayColor,
+       author: {
+         name: `${isBot} ${infoMember.user.username}#${infoMember.user.discriminator}`,
+         icon_url: infoMember.user.avatarURL
+       },
+       title: "ID",
+       description: `${infoMember.user.id}`,
+       thumbnail: {
+         url: infoMember.user.avatarURL
+       },
+       fields: [
+         { name: "Mention",
+           value: `<@${infoMember.user.id}>`,
+           inline: true
+         },
+         { name: "Nickname",
+           value: `${infoMemberNickname}`,
+           inline: true
+         },
+         { name: "Status",
+           value: `${infoMemberStatus}`,
+           inline: true
+         },
+         { name: "Playing",
+           value: `${infoMemberPlaying}`,
+           inline: true
+         },
+         { name: "Accountage",
+           value: `${infoMemberAccountAge} day(s)`,
+           inline: true
+         },
+         { name: "Memberage",
+           value: `${infoMemberMemberAge} day(s)`,
+           inline: true
+         },
+         { name: `Server Deafened`,
+           value: `${infoMember.serverDeaf}`,
+           inline: true
+         },
+         { name: `Server Muted`,
+           value: `${infoMember.serverMute}`,
+           inline: true
+         },
+         { name: "Created On",
+           value: `${infoMember.user.createdAt}`,
+           inline: false
+         },
+         { name: "Join Date",
+           value: `${infoMember.joinedAt}`,
+           inline: false
+         },
+         { name: `Roles [${roleList.length}]`,
+           value: `${roleList.join(", ")}`,
+           inline: false
+         }
+       ]
+     }});
      message.delete(4000);
      return;
     } else {
@@ -97,6 +310,5 @@ exports.run = (client, message, args) => {
       message.guild.me.lastMessage.delete(6000);
     });
     message.delete(4000);
-   }
-   */
+  }
 };
