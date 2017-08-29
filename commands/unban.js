601fd6292ce83357cbce3b0206bfaeaef504c0dc
Timo <timoqueezle@gmail.com>
2017-08-29 20:34:18 +0200

    check banlist for ID before attempting ban

--- commands/unban.js
+++ commands/unban.js
@@ -29,26 +29,36 @@ exports.run = (client, message, [mention, ...reason]) => {
     message.delete(4000);
     return
   }
-  message.guild.unban(`${mention}`)
-    .then(unbanUser => {
-      message.channel.send({
-        embed: {
-          color: message.guild.me.displayColor,
-          description: `:white_check_mark: Okay <@${message.member.id}>, ${unbanUser.username}#${unbanUser.discriminator} <@${unbanUser.id}> (ID:${unbanUser.id}) was succesfully unbanned.`
+  message.guild.fetchBans()
+    .then(
+      banCollection => {
+        console.log("banCollection")
+        console.log(banCollection)
+        if (banCollection.get(`${mention}`) != undefined) {
+          message.guild.unban(`${mention}`)
+            .then(unbanUser => {
+              message.channel.send({
+                embed: {
+                  color: message.guild.me.displayColor,
+                  description: `:white_check_mark: Okay <@${message.member.id}>, ${unbanUser.username}#${unbanUser.discriminator} <@${unbanUser.id}> (ID:${unbanUser.id}) was succesfully unbanned.`
+                }
+              })
+            })
+        } else {
+          message.channel.send({
+              embed: {
+                color: message.guild.me.displayColor,
+                description: `:x: Sorry <@${message.member.id}>, **${mention}** is not a banned ID`
+              }
+            })
+            .then(message => {
+              message.guild.me.lastMessage.delete(6000);
+            });
         }
-      })
-    })
-    .catch(errorMessage => {
-      console.log(errorMessage);
-      message.channel.send({
-          embed: {
-            color: message.guild.me.displayColor,
-            description: `:x: Sorry <@${message.member.id}>, **${mention}** is not a banned ID`
-          }
-        })
-        .then(message => {
-          message.guild.me.lastMessage.delete(6000);
-        });
-    })
+      }
+    )
+    .catch(
+      console.error
+    )
   message.delete(4000);
 };
