f4ccfc563fffc1a2f982380cbf68c0fcb4bc8c4a
Comfy-Mittens <hitsuzen13@gmail.com>
2017-08-29 12:10:25 -0600

    Update ban.js
    Minor rewrite and fix logic when mention doesn't resolve to a user.

--- commands/ban.js
+++ commands/ban.js
@@ -1,90 +1,74 @@
+function embedMessage(color, message) {
+	return {
+		embed: {
+			color: color,
+			description: message
+		}
+	};
+}
+
 exports.run = (client, message, [mention, ...reason]) => {
-  const config = require("../config.json");
-  if (!mention) {
-    message.channel.send({
-      embed: {
-        color: message.guild.me.displayColor,
-        description: `:x: <@${message.member.id}>, please provide a guild member to ban`
-      }
-    }).then(message => {
-      message.guild.me.lastMessage.delete(6000);
-    });
-    message.delete(4000);
-    return;
-  }
-  if (!message.guild.me.hasPermission("BAN_MEMBERS")) {
-    message.channel.send({
-      embed: {
-        color: message.guild.me.displayColor,
-        description: `:x: Sorry <@${message.member.id}>, I don't have the permissions to do that`
-      }
-    }).then(message => {
-      message.guild.me.lastMessage.delete(6000);
-    });
-    message.delete(4000);
-    return
-  }
-  if (!message.member.hasPermission("BAN_MEMBERS")) {
-    console.log(`[!!!] unauthorized command invoke !ban by user <@${message.member.id}>,${message.member.displayName}, ${message.member.user.username}#${message.member.user.discriminator} || on <${mention}>`);
-    message.delete(4000);
-    return
-  }
-  if (message.mentions.users.size === 1) {
-    const banMember = message.mentions.members.first();
-    banMember.ban(reason.join(" ")).then(member => {
-      message.channel.send({
-        embed: {
-          color: message.guild.me.displayColor,
-          description: `:white_check_mark: <@${message.member.id}>, ${banMember.displayName} | <@${banMember.id}> | (ID: ${banMember.id}) was succesfully banned.`
-        }
-      })
-    });
-    message.delete(4000);
-    return;
-  }
-  if (message.guild.members.get(`${mention}`) !== undefined) {
-    const banMember = message.guild.members.get(`${mention}`);
-    banMember.ban(reason.join(" ")).then(member => {
-      message.channel.send({
-        embed: {
-          color: message.guild.me.displayColor,
-          description: `:white_check_mark: <@${message.member.id}>, ${banMember.displayName} | <@${banMember.id}> | (ID: ${banMember.id}) was succesfully banned.`
-        }
-      })
-    });
-    message.delete(4000);
-    return;
-  }
-  if (mention) {
-    var nickUserName = message.guild.members.find(function(member) {
-      if (member.nickname != null) {
-        if (member.nickname.toUpperCase() == mention.toUpperCase())
-          return true;
-      }
-      if (member.user.username.toUpperCase() == mention.toUpperCase())
-        return true;
-    })
-    if (nickUserName != null) {
-      nickUserName.ban(reason.join(" ")).then(member => {
-        message.channel.send({
-          embed: {
-            color: message.guild.me.displayColor,
-            description: `:white_check_mark: <@${message.member.id}>, ${nickUserName.displayName} | <@${nickUserName.id}> | (ID: ${nickUserName.id}) was succesfully banned.`
-          }
-        })
-      });
-      message.delete(4000);
-      return;
-    }
-  } else {
-    message.channel.send({
-      embed: {
-        color: message.guild.me.displayColor,
-        description: `:x: Sorry <@${message.member.id}>, **${mention}** is not a valid guild member`
-      }
-    }).then(message => {
-      message.guild.me.lastMessage.delete(6000);
-    });
-    message.delete(4000);
-  }
+	const config = require("../config.json");
+	if (!mention) {
+		message.channel.send(embedMessage(message.guild.me.displayColor, `:x: <@${message.member.id}>, please provide a guild member to ban`))
+	.then(message => {
+		message.guild.me.lastMessage.delete(6000);
+	});
+	message.delete(4000);
+		return;
+	}
+	if (!message.guild.me.hasPermission("BAN_MEMBERS")) {
+		message.channel.send(embedMessage(message.guild.me.displayColor, `:x: Sorry <@${message.member.id}>, I don't have the permissions to do that`))
+	.then(message => {
+		message.guild.me.lastMessage.delete(6000);
+	});
+		message.delete(4000);
+		return;
+	}
+	if (!message.member.hasPermission("BAN_MEMBERS")) {
+		console.log(`[!!!] unauthorized command invoke !ban by user <@${message.member.id}>,${message.member.displayName}, ${message.member.user.username}#${message.member.user.discriminator} || on <${mention}>`);
+		message.delete(4000);
+		return;
+	}
+	if (message.mentions.users.size === 1) {
+		const banMember = message.mentions.members.first();
+		banMember.ban(reason.join(" "))
+	.then(member => {
+		message.channel.send(embedMessage(message.guild.me.displayColor, `:white_check_mark: <@${message.member.id}>, ${banMember.displayName} | <@${banMember.id}> | (ID: ${banMember.id}) was succesfully banned.`))
+	});
+		message.delete(4000);
+		return;
+	}
+	if (message.guild.members.get(`${mention}`) !== undefined) {
+		const banMember = message.guild.members.get(`${mention}`);
+		banMember.ban(reason.join(" "))
+	.then(member => {
+		message.channel.send(embedMessage(message.guild.me.displayColor, `:white_check_mark: <@${message.member.id}>, ${banMember.displayName} | <@${banMember.id}> | (ID: ${banMember.id}) was succesfully banned.`))
+	});
+		message.delete(4000);
+		return;
+	}
+	var nickUserName = message.guild.members.find(function(member) {
+		if (member.nickname != null) {
+			if (member.nickname.toUpperCase() == mention.toUpperCase())
+				return true;
+		}
+		if (member.user.username.toUpperCase() == mention.toUpperCase())
+			return true;
+	});
+	
+	if (nickUserName != null) {
+	nickUserName.ban(reason.join(" "))
+	.then(member => {
+		message.channel.send(embedMessage(message.guild.me.displayColor, `:white_check_mark: <@${message.member.id}>, ${nickUserName.displayName} | <@${nickUserName.id}> | (ID: ${nickUserName.id}) was succesfully banned.`));
+			});
+		message.delete(4000);
+		return;
+	} else {
+		message.channel.send(embedMessage(message.guild.me.displayColor, `:x: Sorry <@${message.member.id}>, **${mention}** is not a valid guild member`))
+		.then(message => {
+			message.guild.me.lastMessage.delete(6000);
+		});
+		message.delete(4000);
+	}
 };
