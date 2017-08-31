c550f01bbbf185bf742df8ad27695a67bdebc479
Timo <timoqueezle@gmail.com>
2017-08-31 21:59:08 +0200

    functionize things, probably badly, but it works.

--- commands/prune.js
+++ commands/prune.js
@@ -2,6 +2,23 @@ const util = require("../foxxo.util.js");
 const Discord = require("discord.js");
 exports.run = (client, message, [amount, filter]) => {
     amount = Math.min(amount, 100)
+
+    function massDeleteAndLog(message, messages, filteredBy) {
+        if (messages.size > 0) {
+            message.channel.bulkDelete(messages)
+                .then(deletedMessages => {
+                    console.log("can this pls happen?")
+                    message.channel.send(util.createEmbed(message.guild.me.displayColor, `:put_litter_in_its_place: <@${message.member.id}> successfully deleted **${deletedMessages.size}** out of ${amount} checked messages matching: <@${filteredBy}>`)).then(message => {
+                        message.guild.me.lastMessage.delete(6000);
+                    });
+                })
+                .catch(console.error)
+        } else {
+            message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, **0** messages out of ${amount} checked messages matching: <@${filteredBy}>`)).then(message => {
+                message.guild.me.lastMessage.delete(6000);
+            });
+        }
+    }
     if (!message.member.hasPermission("MANAGE_MESSAGES")) {
         console.log(`[!!!] unauthorized command invoke !prune by user <@${message.member.id}>,${message.member.displayName}, ${message.member.user.tag} || on <${amount}> - <${filter}`);
         message.delete(4000);
@@ -62,37 +79,13 @@ exports.run = (client, message, [amount, filter]) => {
                     if (message.mentions.users.size === 1) {
                         filterBy = message.mentions.members.first().id
                         messages = messages.filter(m => m.author.id === filterBy)
-                        if (messages.size > 0) {
-                            message.channel.bulkDelete(messages)
-                                .then(deletedMessages => {
-                                    message.channel.send(util.createEmbed(message.guild.me.displayColor, `:put_litter_in_its_place: <@${message.member.id}> successfully deleted **${deletedMessages.size}** out of ${amount} checked messages matching: <@${filterBy}>`)).then(message => {
-                                        message.guild.me.lastMessage.delete(6000);
-                                    });
-                                })
-                                .catch(console.error)
-                        } else {
-                            message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, **0** messages out of ${amount} checked messages matching: <@${filterBy}>`)).then(message => {
-                                message.guild.me.lastMessage.delete(6000);
-                            });
-                        }
+                        massDeleteAndLog(message, messages, filterBy)
                     } else {
                         client.fetchUser(filter, true)
                             .then(fetchedUser => {
                                 filterBy = fetchedUser.id;
                                 messages = messages.filter(m => m.author.id === filterBy)
-                                if (messages.size > 0) {
-                                    message.channel.bulkDelete(messages)
-                                        .then(deletedMessages => {
-                                            message.channel.send(util.createEmbed(message.guild.me.displayColor, `:put_litter_in_its_place: <@${message.member.id}> successfully deleted **${deletedMessages.size}** out of ${amount} checked messages matching: <@${filterBy}>`)).then(message => {
-                                                message.guild.me.lastMessage.delete(6000);
-                                            });
-                                        })
-                                        .catch(console.error)
-                                } else {
-                                    message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, **0** messages out of ${amount} checked messages matching: <@${filterBy}>`)).then(message => {
-                                        message.guild.me.lastMessage.delete(6000);
-                                    });
-                                }
+                                massDeleteAndLog(message, messages, filterBy)
                             })
                             .catch(error => {
                                 message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, invalid filter`)).then(message => {
