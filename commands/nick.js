051c45e811f23024546b6ca5441763a6de2af27d
Timo <timoqueezle@gmail.com>
2017-09-06 01:00:42 +0200

    introduces !nick to nickname people

---
+++ commands/nick.js
@@ -0,0 +1,41 @@
+const config = require("../config.json");
+const util = require("../foxxo.util.js");
+exports.run = (client, message, [mention, ...nick]) => {
+    if (!message.member.hasPermission("MANAGE_NICKNAMES")) {
+        console.log(`[!!!] unauthorized command invoke !nick by user <@${message.member.id}>,${message.member.displayName}, ${message.member.user.tag} || on <${mention}> <${nick}>`);
+        message.delete(4000)
+        return;
+    }
+    if (!mention) {
+        message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, provide a guild member to kick`))
+            .then(message => {
+                message.guild.me.lastMessage.delete(6000);
+            });
+        message.delete(4000);
+        return;
+    }
+    if (!message.guild.me.hasPermission("MANAGE_NICKNAMES")) {
+        message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, I don't have the permissions to do that`))
+            .then(message => {
+                message.guild.me.lastMessage.delete(6000);
+            });
+        message.delete(4000);
+        return;
+    }
+    var nickMember = util.getGuildMember(mention, message)
+    if (nickMember) {
+        nickMember.setNickname(`${nick}`)
+            .then(member => {
+                message.channel.send(util.createEmbed(message.guild.me.displayColor, `:white_check_mark: <@${message.member.id}> succesfully nicknamed ${nickMember.tag} | <@${nickMember.id}> | ID: ${nickMember.id} to ${nick}`))
+            });
+        message.delete(4000);
+        return;
+
+    } else {
+        message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, **${mention}** is not a valid guild member`))
+            .then(message => {
+                message.guild.me.lastMessage.delete(6000);
+            });
+        message.delete(4000);
+    }
+};
