953cd8093b7d9530281b7599d88786a771414888
Timo <timoqueezle@gmail.com>
2017-09-08 12:39:03 +0200

    adds !prefix

---
+++ commands/prefix.js
@@ -0,0 +1,18 @@
+const config = require("../config.json");
+const util = require("../foxxo.util.js");
+const fs = require("fs")
+exports.run = (client, message, [prefix]) => {
+    if (message.author.id !== config.ownerID && message.author.id !== config.teachID) return;
+    if (!prefix) {
+        message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, provide a prefix to change to`))
+            .then(message => {
+                message.guild.me.lastMessage.delete(6000);
+            });
+        message.delete(4000);
+        return;
+    }
+    config.prefix = prefix;
+    message.channel.send(util.createEmbed(message.guild.me.displayColor, `:white_check_mark: <@${message.member.id}> succesfully changed prefix to ${prefix}`))
+    message.delete(4000);
+    fs.writeFile("./config.json", JSON.stringify(config, null, 4), (err) => console.error);
+};
