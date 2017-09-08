c0b9697139d2253a77f465c14915b78fb83fe1c7
Timo <timoqueezle@gmail.com>
2017-09-08 12:38:54 +0200

    adds foxes, reboot for !addfox

---
+++ commands/addfox.js
@@ -0,0 +1,27 @@
+const config = require("../config.json");
+const foxconfig = require("../ranfoxconfig.json");
+const util = require("../foxxo.util.js");
+const fs = require("fs")
+exports.run = (client, message, [link]) => {
+    if (message.author.id !== config.ownerID && message.author.id !== config.teachID) return;
+    if (!link) {
+        message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, provide a link to add`))
+            .then(message => {
+                message.guild.me.lastMessage.delete(6000);
+            });
+        message.delete(4000);
+        return;
+    }
+    foxconfig.foxes.push(link);
+    message.channel.send({
+        embed: {
+            color: message.guild.me.displayColor,
+            description: `:fox: <@${message.member.id}> added a fox`,
+            thumbnail: {
+                url: link
+            },
+        }
+    })
+    message.delete(4000);
+    fs.writeFile("./ranfoxconfig.json", JSON.stringify(foxconfig, null, 4), (err) => console.error);
+};
