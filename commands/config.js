4312cf651153fa0e1633ff3e8028110bc985cb0a
Timo <timoqueezle@gmail.com>
2017-09-13 23:47:40 +0200

    config command to change config file contents

--- commands/config.js
+++ commands/config.js
@@ -1,13 +1,13 @@
 const config = require("../config/config.json");
 const util = require("../foxxo.util.js");
 const Discord = require("discord.js");
+const fs = require("fs")
 exports.run = (client, message, args) => {
-    console.log(config.list)
     var list = args[0];
     var option = args[1];
     var mention = args[2];
-    var viableLists = ["blacklist", "whitelist", "owner"]
-    var viableOptions = ["add", "remove", "+", "-"]
+    var viableLists = ["blacklist", "whitelist", "owner"];
+    var viableOptions = ["add", "remove", "+", "-"];
     // check if invoking user is owner
     if (!config.owner.includes(`${message.author.id}`)) {
         return;
@@ -44,14 +44,26 @@ exports.run = (client, message, args) => {
         .then(fetchedUser => {
             console.log(fetchedUser.tag)
             // if user is found and fetched
-            // check if user is already on provided list (checks all lists)
-            if (config.list.includes(fetchedUser.id)) {
-                message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, <@${fetchedUser.id}> is already on "${list}"`))
-                    .then(message => {
-                        message.guild.me.lastMessage.delete(6000);
-                    });
-                message.delete(4000);
-                return;
+            // check if user is already on provided list
+            if (option == "add" || option == "+") {
+                if (config[list].includes(fetchedUser.id)) {
+                    message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, <@${fetchedUser.id}> is already on "${list}"`))
+                        .then(message => {
+                            message.guild.me.lastMessage.delete(6000);
+                        });
+                    message.delete(4000);
+                    return;
+                }
+            }
+            if (option == "remove" || option == "-") {
+                if (!config[list].includes(fetchedUser.id)) {
+                    message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, <@${fetchedUser.id}> is not on "${list}"`))
+                        .then(message => {
+                            message.guild.me.lastMessage.delete(6000);
+                        });
+                    message.delete(4000);
+                    return;
+                }
             }
             // find things out
             var pin = util.getRandInt(1000, 9999);
@@ -59,25 +71,25 @@ exports.run = (client, message, args) => {
             const isWhitelisted = config.whitelist.includes(`${fetchedUser.id}`) == true ? "[🔑Whitelist]" : ""
             const isBlacklisted = config.blacklist.includes(`${fetchedUser.id}`) == true ? "[⛔Blacklist]" : ""
             const isOwner = config.owner.includes(`${fetchedUser.id}`) == true ? "[🦊Owner]" : ""
-            const ignoreMemberStatus = fetchedUser.presence.status.charAt(0).toUpperCase() + fetchedUser.presence.status.slice(1);
-            const ignoreMemberPlaying = fetchedUser.presence.game == null ? "None" : fetchedUser.presence.game
-            var ignoreMemberAccountAge = Math.floor((Date.now() - fetchedUser.createdAt) / 1000 / 60 / 60 / 24);
+            const fetchedUserStatus = fetchedUser.presence.status.charAt(0).toUpperCase() + fetchedUser.presence.status.slice(1);
+            const fetchedUserPlaying = fetchedUser.presence.game == null ? "None" : fetchedUser.presence.game
+            var fetchedUserAccountAge = Math.floor((Date.now() - fetchedUser.createdAt) / 1000 / 60 / 60 / 24);
             message.channel.send({
                     embed: {
                         color: 16426522,
                         title: `Confirmation needed - Config - ${list} - ${option}`,
-                        description: `:question: <@${message.member.id}>, you are about to change this users config with the following arguments: list: ${list} | option: ${option}\r\n\r\nConfirm ignore with pin **${pin}**, refuse with \`cancel\`\r\n`,
+                        description: `:question: <@${message.member.id}>, you are about to change this users config with the following arguments: list: ${list} | option: ${option}\r\n\r\nConfirm with pin **${pin}**, refuse with \`cancel\`\r\n`,
                         thumbnail: {
-                            url: ignoreMember.avatarURL
+                            url: fetchedUser.avatarURL
                         },
                         fields: [{
                                 name: `Userinformation`,
-                                value: `**Usertag:** ${isWhitelisted}${isBlacklisted}${isBot} ${ignoreMember.tag}\r\n**ID:** ${ignoreMember.id}\r\n**Mention:** <@${ignoreMember.id}>\r\n\r\n**Status:** ${ignoreMemberStatus}\r\n**Playing:** ${ignoreMemberPlaying}`,
+                                value: `**Usertag:** ${isWhitelisted}${isBlacklisted}${isBot} ${fetchedUser.tag}\r\n**ID:** ${fetchedUser.id}\r\n**Mention:** <@${fetchedUser.id}>\r\n\r\n**Status:** ${fetchedUserStatus}\r\n**Playing:** ${fetchedUserPlaying}`,
                                 inline: false
                             },
                             {
                                 name: "Accountinformation",
-                                value: `**Accountage:** ${ignoreMemberAccountAge}`,
+                                value: `**Accountage:** ${fetchedUserAccountAge}`,
                                 inline: false
                             }
                         ],
@@ -94,17 +106,19 @@ exports.run = (client, message, args) => {
                             errors: [`time`],
                         })
                         .then(collectedMsg => {
+                            console.log(collectedMsg.first().content)
                             if (collectedMsg.first().content === `${pin}`) {
+                                console.log("checkin for the pin")
                                 if (option == "add" || option == "+") {
-                                    config.list.push(fetchedUser.id);
+                                    config[list].push(fetchedUser.id);
                                     fs.writeFile("./config/config.json", JSON.stringify(config, null, 4), (err) => console.error);
-                                    message.channel.send(util.createEmbed(message.guild.me.displayColor, `:white_check_mark: <@${message.member.id}> succesfully changed config for ${ignoreMember.tag} | <@${ignoreMember.id}> | ID: ${ignoreMember.id} with the following arguments: list: ${list} | option: ${option}`));
+                                    message.channel.send(util.createEmbed(message.guild.me.displayColor, `:white_check_mark: <@${message.member.id}> succesfully changed config for ${fetchedUser.tag} | <@${fetchedUser.id}> | ID: ${fetchedUser.id} with the following arguments: list: ${list} | option: ${option}`));
                                 }
                                 if (option == "remove" || option == "-") {
-                                    var index = config.list.indexOf(fetchedUser.id);
-                                    config.ignoredUsers.splice(index, 1);
+                                    var index = config[list].indexOf(fetchedUser.id);
+                                    config[list].splice(index, 1);
                                     fs.writeFile("./config/config.json", JSON.stringify(config, null, 4), (err) => console.error);
-                                    message.channel.send(util.createEmbed(message.guild.me.displayColor, `:white_check_mark: <@${message.member.id}> succesfully changed config for ${ignoreMember.tag} | <@${ignoreMember.id}> | ID: ${ignoreMember.id} with the following arguments: list: ${list} | option: ${option}`));
+                                    message.channel.send(util.createEmbed(message.guild.me.displayColor, `:white_check_mark: <@${message.member.id}> succesfully changed config for ${fetchedUser.tag} | <@${fetchedUser.id}> | ID: ${fetchedUser.id} with the following arguments: list: ${list} | option: ${option}`));
                                 }
                             } else {
                                 message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, config change request canceled`))
@@ -117,6 +131,7 @@ exports.run = (client, message, args) => {
                             message.delete(4000);
                         })
                         .catch(e => {
+                            console.log(e)
                             message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, config change request canceled`))
                                 .then(message => {
                                     message.guild.me.lastMessage.delete(6000);
