b79af36fca93a89060cea5df8b6e872e2a0cc8aa
Timo <timoqueezle@gmail.com>
2017-09-14 00:43:36 +0200

    adds new filepath for config files to !reload

--- commands/reload.js
+++ commands/reload.js
@@ -2,7 +2,7 @@ const config = require("../config/config.json");
 const util = require("../foxxo.util.js");
 exports.run = (client, message, args) => {
     var extensions = [".js", ".json"];
-    var folders = ["./", "../"];
+    var folders = ["./", "../", "../config/"];
 
     for (var i in extensions) {
         for (var j in folders) {
