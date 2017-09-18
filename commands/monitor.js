82ec7cb3c2fc65233f284cc6e9da51c9c4af6955
Timo <timoqueezle@gmail.com>
2017-09-18 14:21:00 +0200

    ploishes + fixes for voice monitoring

--- commands/monitor.js
+++ commands/monitor.js
@@ -16,13 +16,13 @@ exports.run = (client, message, args) => {
         // show current monitors
         var listMonitors = []
         for (i in monitorconfig.voicechannels) {
-            // only push monitorconfig.voicechannels entries for channels on the guild the command is invoked in
+            // only push monitorconfig.voicechannels entries for channels on the guild the command is invoked in or true argument is givens
             if (option == "true" || message.guild.channels.get(monitorconfig.voicechannels[i].voiceID)) {
                 listMonitors.push(`[${i}] <#${monitorconfig.voicechannels[i].voiceID}> => <#${monitorconfig.voicechannels[i].textID}>`)
             }
         }
         message.channel.send(util.createEmbed(16426522, `:information_source:  <@${message.member.id}>, Active monitors:\r\n\r\n${listMonitors.length == 0 ? "None" : listMonitors.join("\r\n")}`));
-        message.delete(4000);
+        message.delete(4000)
         return;
     }
     // check if option is valid with array
@@ -31,7 +31,7 @@ exports.run = (client, message, args) => {
             .then(message => {
                 message.guild.me.lastMessage.delete(6000);
             });
-        message.delete(4000);
+        message.delete(4000)
         return;
     }
     // check options, if add
@@ -42,7 +42,7 @@ exports.run = (client, message, args) => {
                 .then(message => {
                     message.guild.me.lastMessage.delete(6000);
                 });
-            message.delete(4000);
+            message.delete(4000)
             return;
         } else if (!client.channels.get(monitorVoice) || !client.channels.get(reportIn)) {
             // if channel ID is not a valid channel, send error
@@ -50,7 +50,7 @@ exports.run = (client, message, args) => {
                 .then(message => {
                     message.guild.me.lastMessage.delete(6000);
                 });
-            message.delete(4000);
+            message.delete(4000)
             return;
         }
         // add the setup to config
@@ -61,31 +61,38 @@ exports.run = (client, message, args) => {
         //write the file, send confirmation
         fs.writeFile("./config/monitorconfig.json", JSON.stringify(monitorconfig, null, 4), (err) => console.error);
         message.channel.send(util.createEmbed(16426522, `:white_check_mark: <@${message.member.id}> succesfully added monitor for <#${monitorVoice}>, reporting in <#${reportIn}>`));
+        message.delete(4000)
+        return;
     }
     // if option is remove
     if (option == "remove" || option == "-") {
         // for every list entry in monitorconfig.voicechannels
+        var found = 0
         for (i in monitorconfig.voicechannels) {
             if (monitorconfig.voicechannels[i].voiceID == monitorVoice) {
-                // when monitorVoice matches a .voiceID entry remove it, write file, send confirmation
+                //if monitorVoice matches a .voiceID entry remove it, write file, send confirmation
                 message.channel.send(util.createEmbed(16426522, `:white_check_mark: <@${message.member.id}> succesfully removed monitor for <#${monitorconfig.voicechannels[i].voiceID}>, reporting in <#${monitorconfig.voicechannels[i].textID}>`));
                 monitorconfig.voicechannels.splice(i, 1);
                 fs.writeFile("./config/config.json", JSON.stringify(config, null, 4), (err) => console.error);
-                return;
+                found = 1;
             }
             if (monitorconfig.voicechannels[monitorVoice]) {
                 // if it's not a .voiceID match, try to interpret it as array index, remove it, write file, send confirmation
                 message.channel.send(util.createEmbed(16426522, `:white_check_mark: <@${message.member.id}> succesfully removed monitor for <#${monitorconfig.voicechannels[monitorVoice].voiceID}>, reporting in <#${monitorconfig.voicechannels[monitorVoice].textID}>`));
                 monitorconfig.voicechannels.splice(monitorVoice, 1)
                 fs.writeFile("./config/monitorconfig.json", JSON.stringify(monitorconfig, null, 4), (err) => console.error);
-            } else {
-                message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, no list entry found or invalid ID provided`))
-                    .then(message => {
-                        message.guild.me.lastMessage.delete(6000);
-                    });
-                message.delete(4000);
-                return;
+                found = 1;
+                // break for-loop if one is found to prevent the loop from removing all entries
+                break;
             }
         }
+        if (found == 0) {
+            message.channel.send(util.createEmbed(message.guild.me.displayColor, `:x: <@${message.member.id}>, no list entry found or invalid ID provided`))
+                .then(message => {
+                    message.guild.me.lastMessage.delete(6000);
+                });
+        }
     }
+    message.delete(4000)
+    return;
 }
--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -5,12 +5,9 @@ const util = require("../foxxo.util.js");
 exports.run = (client, oldMember, newMember) => {
     for (i in monitorconfig.voicechannels) {
         if (newMember.voiceChannel && monitorconfig.voicechannels[i].voiceID == newMember.voiceChannel.id) {
-            newMember.guild.channels.get(monitorconfig.voicechannels[i].textID).send(util.createEmbed(newMember.guild.me.displayColor, `:sound: <@${newMember.id}> joined <#${monitorconfig.voicechannels[i].voiceID}>`))
-            return;
-        }
-        if (oldMember.voiceChannel && !newMember.voiceChannel) {
-            oldMember.guild.channels.get(monitorconfig.voicechannels[i].textID).send(util.createEmbed(oldMember.guild.me.displayColor, `:mute: <@${oldMember.id}> left <#${monitorconfig.voicechannels[i].voiceID}>`))
-            return;
+            client.channels.get(monitorconfig.voicechannels[i].textID).send(util.createEmbed(client.channels.get(monitorconfig.voicechannels[i].textID).guild.me.displayColor, `:sound: <@${newMember.id}> joined <#${monitorconfig.voicechannels[i].voiceID}>`))
+        } else if (oldMember.voiceChannel && monitorconfig.voicechannels[i].voiceID == oldMember.voiceChannel.id) {
+            client.channels.get(monitorconfig.voicechannels[i].textID).send(util.createEmbed(client.channels.get(monitorconfig.voicechannels[i].textID).guild.me.displayColor, `:mute: <@${oldMember.id}> left <#${monitorconfig.voicechannels[i].voiceID}>`))
         }
     }
 }
