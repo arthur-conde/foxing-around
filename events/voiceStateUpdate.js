d81f5239bf3642891585cb2a8be99ee8e53e7837
Timo <timoqueezle@gmail.com>
2017-09-19 12:17:12 +0200

    changes symbols for join/leave in monitor to not be confused with mute/unmute

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -5,9 +5,9 @@ const util = require("../foxxo.util.js");
 exports.run = (client, oldMember, newMember) => {
     for (i in monitorconfig.voicechannels) {
         if (newMember.voiceChannel && monitorconfig.voicechannels[i].voiceID == newMember.voiceChannel.id) {
-            client.channels.get(monitorconfig.voicechannels[i].textID).send(util.createEmbed(client.channels.get(monitorconfig.voicechannels[i].textID).guild.me.displayColor, `:sound: <@${newMember.id}> joined <#${monitorconfig.voicechannels[i].voiceID}>`))
+            client.channels.get(monitorconfig.voicechannels[i].textID).send(util.createEmbed(client.channels.get(monitorconfig.voicechannels[i].textID).guild.me.displayColor, `:inbox_tray:  <@${newMember.id}> joined <#${monitorconfig.voicechannels[i].voiceID}>`))
         } else if (oldMember.voiceChannel && monitorconfig.voicechannels[i].voiceID == oldMember.voiceChannel.id) {
-            client.channels.get(monitorconfig.voicechannels[i].textID).send(util.createEmbed(client.channels.get(monitorconfig.voicechannels[i].textID).guild.me.displayColor, `:mute: <@${oldMember.id}> left <#${monitorconfig.voicechannels[i].voiceID}>`))
+            client.channels.get(monitorconfig.voicechannels[i].textID).send(util.createEmbed(client.channels.get(monitorconfig.voicechannels[i].textID).guild.me.displayColor, `:outbox_tray:  <@${oldMember.id}> left <#${monitorconfig.voicechannels[i].voiceID}>`))
         }
     }
 }
