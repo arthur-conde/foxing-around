8d7606dfb42a4b2737aa9c36af2d582f2af0b9ba
Timo <timoqueezle@gmail.com>
2017-09-20 17:50:13 +0200

    it does a thing now?

--- commands/dothething.js
+++ commands/dothething.js
@@ -2,27 +2,33 @@ const path = require("path");
 const util = require("util");
 
 exports.run = (client, message, args) => {
-	let command = args.shift();
-	var argObject = {};
-	for(var i in args) {
-		var individualArgument = args[i];
-		var argumentParts = individualArgument.split("=");
-		if (argumentParts.length == 2)
-		{
-			argObject[argumentParts[0].trim()] = argumentParts[1].trim();
-		}
-	}
-	
-	if (Object.keys(argObject).length == 0)
-		argObject = null;
-	
-	try {
-		// let commandFile = require(`./${path.basename(__filename)}/${command}.js`); 
-		// commandFile.run(client, message, args, argObj);
-		message.channel.send(`Attempting to invoke './${path.basename(__filename)}/${command}.js'.
-	Values: (client, message, "{util.inspect(args, {depth: null})}", "{util.inspect(argObj, {depth: null})}")`);
-    } catch (err) {
+    let command = args.shift();
+    var argObject = {};
+    for (var i in args) {
+        var individualArgument = args[i];
+        var argumentParts = individualArgument.split("=");
+        if (argumentParts.length == 2) {
+            argObject[argumentParts[0].trim()] = argumentParts[1].trim();
+        }
+    }
+
+    if (Object.keys(argObject).length == 0)
+        argObject = null;
+
+    try {
+        message.channel.send(`Attempting to invoke './${path.basename(__filename)}/${command}.js'.
+	    Values: (client, message, "${strArgs}", "${strObj}")`);
+        var commandFile = require(`./${path.basename(__filename)}/${command}.js`);
+        commandFile.run(client, message, args, argObj);
+        var strArgs = util.inspect(args, {
+            depth: null
+        });
+        var strObj = util.inspect(argObj, {
+            depth: null
+        });
         // commented for now as to not spam console when other bots are called
-        //console.error(err); // If no command is found, log the error
+        console.error(err); // If no command is found, log the error
+    } catch (err) {
+        console.error(err);
     }
 };
