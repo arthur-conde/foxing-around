a21f322ea89e3703025861f492f8de9e8c0f8d49
Timo <timoqueezle@gmail.com>
2017-09-21 12:40:43 +0200

    note to self: save before commiting

--- foxxo.util.js
+++ foxxo.util.js
@@ -233,6 +233,114 @@ function createChannelEmbed(channel, message) {
     }
     return embed;
 }
+// return member EMBED for provided member (member! not identifier/name/user)
+function createUserEmbed(guildmember, message) {
+    // finding out stuff to later display in the embed
+    // check if bot/admin
+    const isBot = guildmember.user.bot == true ? "🤖" : "";
+    const isWhitelisted = config.whitelist.includes(`${guildmember.id}`) == true ? "[🔑Whitelist]" : ""
+    const isBlacklisted = config.blacklist.includes(`${guildmember.id}`) == true ? "[⛔Blacklist]" : ""
+    const isOwner = config.owner.includes(`${guildmember.id}`) == true ? "[🦊Owner]" : ""
+    // check if Nickname is present, if not display "None" since value in field is required
+    const passedUserNickname = guildmember.nickname != null ? `${guildmember.nickname}` : "None"
+    // Uppercase first letter of status
+    const passedUserStatus = guildmember.user.presence.status.charAt(0).toUpperCase() + guildmember.user.presence.status.slice(1);
+    // display "None" if user playing status isn't set since value in field is required
+    const passedUserPlaying = guildmember.user.presence.game != null ? `${guildmember.user.presence.game.name}` : "None"
+    // calculate ages
+    let passedUserAccountAge = Math.floor((Date.now() - guildmember.user.createdAt) / 1000 / 60 / 60 / 24);
+    let passedUserMemberAge = Math.floor((Date.now() - guildmember.joinedAt) / 1000 / 60 / 60 / 24);
+    // rolelist, push everything BUT everyone (guild ID = role ID)
+    let roleList = [];
+    guildmember.roles.forEach(role => {
+        if (message.guild.id == role.id) {
+            return;
+        } else {
+            roleList.push(role.name)
+        }
+    });
+    // display list length in variable, so no roles can display as 0
+    let roleListLength = roleList.length
+    // display "None" if rolelist is empty since value in field is required
+    roleList = roleList.length === 0 ? ["None"] : roleList
+    roleListLength = roleList.length === 0 ? "0" : roleListLength
+    // define embed
+    embed = {
+        embed: {
+            color: guildmember.displayColor,
+            author: {
+                name: `${isOwner} ${isWhitelisted}${isBlacklisted}${isBot} ${guildmember.user.tag}`,
+                icon_url: guildmember.user.avatarURL
+            },
+            title: "ID",
+            description: `${guildmember.user.id}`,
+            thumbnail: {
+                url: guildmember.user.avatarURL
+            },
+            fields: [{
+                    name: "Mention",
+                    value: `<@${guildmember.user.id}>`,
+                    inline: true
+                },
+                {
+                    name: "Nickname",
+                    value: `${passedUserNickname}`,
+                    inline: true
+                },
+                {
+                    name: "Status",
+                    value: `${passedUserStatus}`,
+                    inline: true
+                },
+                {
+                    name: "Playing",
+                    value: `${passedUserPlaying}`,
+                    inline: true
+                },
+                {
+                    name: "Accountage",
+                    value: `${passedUserAccountAge} day(s)`,
+                    inline: true
+                },
+                {
+                    name: "Memberage",
+                    value: `${passedUserMemberAge} day(s)`,
+                    inline: true
+                },
+                {
+                    name: `Server Deafened`,
+                    value: `${guildmember.serverDeaf}`,
+                    inline: true
+                },
+                {
+                    name: `Server Muted`,
+                    value: `${guildmember.serverMute}`,
+                    inline: true
+                },
+                {
+                    name: "Created On",
+                    value: `${guildmember.user.createdAt.toISOString().slice(0,10)}`,
+                    inline: true
+                },
+                {
+                    name: "Join Date",
+                    value: `${guildmember.joinedAt.toISOString().slice(0,10)}`,
+                    inline: true
+                },
+                {
+                    name: `Roles [${roleListLength}]`,
+                    value: `${roleList.join(", ")}`,
+                    inline: false
+                }
+            ],
+            footer: {
+                text: `Request by ${message.member.displayName}(${message.author.id})`,
+                icon_url: message.author.avatarURL
+            }
+        }
+    }
+    return embed;
+}
 // exports
 exports.createEmbed = (color, message) => createEmbed(color, message);
 exports.createRoleEmbed = (role, message) => createRoleEmbed(role, message);
