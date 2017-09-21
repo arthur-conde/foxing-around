3a94a567cea859a4ecd9ca830de2c741cdda515f
Timo <timoqueezle@gmail.com>
2017-09-21 10:22:27 +0200

    correct the obnoxious mistake the ready message has... finally.

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 const config = require("../config/config.json");
 exports.run = (client) => {
     client.user.setGame(`prefix: ${config.prefix}`, "https://www.google.com/")
-    console.log(`Ready to server in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
+    console.log(`Ready in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
 }
